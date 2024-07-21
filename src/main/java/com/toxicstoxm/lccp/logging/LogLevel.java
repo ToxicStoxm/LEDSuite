0f1a8f6bb92f0dd79364b08e4947dc713ab5d8e5
ToxicStoxm <111985746+ToxicStoxm@users.noreply.github.com>
2024-07-21 16:27:51 +0200

    Delete src directory

--- src/main/java/com/toxicstoxm/lccp/logging/LogLevel.java
+++
@@ -1,6 +0,0 @@
-package com.toxicstoxm.lccp.logging;
-
-// LogLevel interface used by logger.log_level
-public interface LogLevel {
-    boolean isEnabled();
-}
--- src/main/java/com/toxicstoxm/lccp/task_scheduler/LCCPWorker.java
+++
@@ -1,7 +0,0 @@
-package com.toxicstoxm.lccp.task_scheduler;
-
-public interface LCCPWorker {
-    int getTaskId();
-
-    Thread getThread();
-}

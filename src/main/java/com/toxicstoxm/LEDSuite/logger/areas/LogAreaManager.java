f92d4dd09a509d3c8e2ddae03df808af0a47b2cb
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-08-30 01:39:08 +0200

    App rewrite - Added functions to directly get the color of an area by name, to enable / disable areas and a function to get the raw entry set of all registered log areas

--- src/main/java/com/toxicstoxm/LEDSuite/logger/areas/LogAreaManager.java
+++ src/main/java/com/toxicstoxm/LEDSuite/logger/areas/LogAreaManager.java
@@ -1,9 +1,37 @@
 package com.toxicstoxm.LEDSuite.logger.areas;
 
+
+import java.awt.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 public interface LogAreaManager {
     void registerArea(LogArea logArea);
     default void registerAreaBundle(LogAreaBundle logAreaBundle) {
         logAreaBundle.getAreas().forEach(this::registerArea);
+    }
+    Color getAreaColor(String area);
+    boolean enableArea(String string);
+    default boolean enableArea(LogArea logArea) {
+        return enableArea(logArea.getName());
+    }
+    boolean disableArea(String area);
+    default boolean disableArea(LogArea logArea) {
+        return disableArea(logArea.getName());
+    }
+
+    default void enableAreasByName(Collection<String> areas) {
+        areas.forEach(this::enableArea);
+    }
+    default void disableAreas(Collection<String> areas) {
+        areas.forEach(this::disableArea);
+    }
+    default void enableAreas(Collection<LogArea> areas) {
+        areas.forEach(logArea -> enableArea(logArea.getName()));
+    }
+    default void disableAreasByName(Collection<LogArea> areas) {
+        areas.forEach(logArea -> disableArea(logArea.getName()));
     }
     boolean isAreaEnabled(String area);
     default  boolean isAreaEnabled(LogArea area) {
@@ -14,4 +42,5 @@ public interface LogAreaManager {
         return configureArea(area.getName());
     }
     boolean unregisterArea(LogArea logArea);
+    Set<Map.Entry<String, LogArea>> entrySet();
 }

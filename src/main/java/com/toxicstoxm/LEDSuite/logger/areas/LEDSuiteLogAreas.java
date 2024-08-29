0cbcaa305890135dd8e77727ff26514b10906205
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-08-30 01:34:34 +0200

    App rewrite - Changed log area declaration syntax

--- src/main/java/com/toxicstoxm/LEDSuite/logger/areas/LEDSuiteLogAreas.java
+++ src/main/java/com/toxicstoxm/LEDSuite/logger/areas/LEDSuiteLogAreas.java
@@ -1,41 +1,51 @@
 package com.toxicstoxm.LEDSuite.logger.areas;
 
+import lombok.NonNull;
+
 import java.awt.*;
 import java.util.List;
 
 public class LEDSuiteLogAreas implements LogAreaBundle {
 
-    public static class General extends LEDSuiteLogArea {
-        public General() {
+    public static class GENERAL extends LEDSuiteLogArea {
+        public GENERAL() {
             super(new Color(0, 140, 255));
         }
     }
 
-    public static class Network extends LEDSuiteLogArea {}
-
-    public static class YAMLEvents extends LEDSuiteLogArea {
-        public YAMLEvents() {
-            super(List.of(new Network().getName()));
+    public static class NETWORK extends LEDSuiteLogArea {
+        public NETWORK() {
+            super(new Color(255, 0, 0));
         }
     }
 
-    public static class Communication extends LEDSuiteLogArea {
-        public Communication() {
-            super(List.of(new Network().getName()));
+    public static class YAML_EVENTS extends LEDSuiteLogArea {
+        public YAML_EVENTS() {
+            super(new Color(153, 0, 255), List.of(new NETWORK().getName()));
         }
     }
 
-    public static class UI extends LEDSuiteLogArea {}
-
-    public static class UserInteractions extends LEDSuiteLogArea {
-        public UserInteractions() {
-            super(List.of(new UI().getName()));
+    public static class COMMUNICATION extends LEDSuiteLogArea {
+        public COMMUNICATION() {
+            super(new Color(122, 255, 0), List.of(new NETWORK().getName()));
         }
     }
 
-    public static class UIConstruction extends LEDSuiteLogArea {
-        public UIConstruction() {
-            super(List.of(new UI().getName()));
+    public static class UI extends LEDSuiteLogArea {
+        public UI() {
+            super(new Color(255, 106, 0));
+        }
+    }
+
+    public static class USER_INTERACTIONS extends LEDSuiteLogArea {
+        public USER_INTERACTIONS() {
+            super(new Color(2, 253, 217), List.of(new UI().getName()));
+        }
+    }
+
+    public static class UI_CONSTRUCTION extends LEDSuiteLogArea {
+        public UI_CONSTRUCTION() {
+            super(new Color(0, 4, 255), List.of(new UI().getName()));
         }
     }
 }

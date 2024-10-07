6276f33bf2b9c315b1d3346dc3a8adf1b8fec6d6
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-07 21:44:19 +0200

    Added project link and issues link

--- src/main/java/com/toxicstoxm/LEDSuite/Constants.java
+++ src/main/java/com/toxicstoxm/LEDSuite/Constants.java
@@ -32,5 +32,7 @@ public class Constants {
 
     public static class Application {
         public static final String NAME = "LEDSuite";
+        public static final String WEBSITE = "https://github.com/ToxicStoxm/LEDSuite";
+        public static final String ISSUES = WEBSITE + "/issues";
     }
 }

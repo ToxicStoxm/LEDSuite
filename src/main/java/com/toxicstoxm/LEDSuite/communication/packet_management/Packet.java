3ca2258c2f1f549eb72e8d67cbc293ff0edfb545
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-20 21:51:47 +0200

    Added get packet identifier and handle packet methods. Added documentation

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/Packet.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/Packet.java
@@ -1,8 +1,60 @@
 package com.toxicstoxm.LEDSuite.communication.packet_management;
 
-public interface Packet {
-    String getPacketType();
+import com.toxicstoxm.LEDSuite.logger.LEDSuiteLogAreas;
+import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
 
-    Packet deserialize(String yamlString);
+/**
+ *
+ * @since 1.0.0
+ */
+public interface Packet {
+
+    /**
+     * The packets general type. E.g. {@code request} , {@code reply}, {@code error}, ...
+     * @return the packets general type
+     */
+    String getType();
+
+    /**
+     * The packet-subtype. E.g. {@code status request}, {@code menu reply}, {@code parsing error}, ...
+     * @return the packets subtype / more specific type
+     */
+    String getSubType();
+
+    /**
+     * The packet identifier (packet-type.packet-subtype). Combination of {@link #getType()} and {@link #getSubType()}.
+     * @return the packets identifier
+     */
+    default String getIdentifier() {
+
+        String type = getType();
+        String subType = getSubType();
+
+        if (subType == null || subType.isBlank()) return type;
+
+        return type + "." + subType;
+    }
+
+    /**
+     * Attempts to deserialize a given YAML string.
+     * @param yamlString YAML string to deserialize
+     * @return the deserialized packet
+     * @throws PacketManager.DeserializationException if deserialization fails, because invalid YAML or missing values
+     * @see #serialize()
+     */
+    Packet deserialize(String yamlString) throws PacketManager.DeserializationException;
+
+    /**
+     * Attempts to serialize this packet.
+     * @return the serialized string
+     * @see #deserialize(String)
+     */
     String serialize();
+
+    /**
+     *
+     */
+    default void handlePacket() {
+        LEDSuiteApplication.getLogger().warn("Implementation for handling packet type '" + getIdentifier() + "' wasn't found!", new LEDSuiteLogAreas.COMMUNICATION());
+    }
 }

6f6ede08dbceeaee9cb4b60a7404c9820c2da7b6
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-20 21:59:23 +0200

    Added basic logic to handle incoming packets

--- src/main/java/com/toxicstoxm/LEDSuite/communication/websocket/WebSocketCommunication.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/websocket/WebSocketCommunication.java
@@ -1,5 +1,7 @@
 package com.toxicstoxm.LEDSuite.communication.websocket;
 
+import com.toxicstoxm.LEDSuite.communication.packet_management.CommunicationPacket;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.requests.StatusRequestPacket;
 import com.toxicstoxm.LEDSuite.logger.LEDSuiteLogAreas;
 import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
 import jakarta.websocket.*;
@@ -10,11 +12,22 @@ public class WebSocketCommunication extends WebSocketClientEndpoint {
     @OnOpen
     public void onOpen(Session session) {
         LEDSuiteApplication.getLogger().info("WebSocket connection opened with session ID: " + session.getId(), new LEDSuiteLogAreas.NETWORK());
+
+        LEDSuiteApplication.getWebSocketCommunication().enqueueMessage(
+                StatusRequestPacket.builder().build().serialize()
+        );
+
     }
 
     @OnMessage
     public void onMessage(String message, Session session) {
-        LEDSuiteApplication.getLogger().info("Received message from session ID " + session.getId() + ": " + message, new LEDSuiteLogAreas.COMMUNICATION());
+        LEDSuiteApplication.getLogger().info("Received message from session ID " + session.getId(), new LEDSuiteLogAreas.COMMUNICATION());
+
+        // deserialize the incoming packet
+        CommunicationPacket incomingPacket = LEDSuiteApplication.getPacketManager().deserialize(CommunicationPacket.class, message);
+
+        // handle the incoming packet using the packet received handler
+        LEDSuiteApplication.getPacketReceivedHandler().handleIncomingPacket(incomingPacket);
     }
 
     @OnClose

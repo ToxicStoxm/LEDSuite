006b0e707ee832e3eb5d194da60a9aeb35a20cbb
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-20 17:01:26 +0200

    Changed log area colors and renamed YAML_EVENTS to YAML for clarity

--- src/main/java/com/toxicstoxm/LEDSuite/logger/LEDSuiteLogAreas.java
+++ src/main/java/com/toxicstoxm/LEDSuite/logger/LEDSuiteLogAreas.java
@@ -10,43 +10,43 @@ public class LEDSuiteLogAreas implements LogAreaBundle {
 
     public static class GENERAL extends YAJLLogArea {
         public GENERAL() {
-            super(new Color(0, 140, 255));
+            super(new Color(85, 85, 85));
         }
     }
 
     public static class NETWORK extends YAJLLogArea {
         public NETWORK() {
-            super(new Color(255, 0, 0));
+            super(new Color(0, 149, 156));
         }
     }
 
-    public static class YAML_EVENTS extends YAJLLogArea {
-        public YAML_EVENTS() {
-            super(new Color(153, 0, 255), List.of(new NETWORK().getName()));
+    public static class YAML extends YAJLLogArea {
+        public YAML() {
+            super(new Color(0, 89, 255), List.of(new NETWORK().getName()));
         }
     }
 
     public static class COMMUNICATION extends YAJLLogArea {
         public COMMUNICATION() {
-            super(new Color(122, 255, 0), List.of(new NETWORK().getName()));
+            super(new Color(0, 38, 151), List.of(new NETWORK().getName()));
         }
     }
 
     public static class UI extends YAJLLogArea {
         public UI() {
-            super(new Color(255, 106, 0));
+            super(new Color(84, 0, 184));
         }
     }
 
     public static class USER_INTERACTIONS extends YAJLLogArea {
         public USER_INTERACTIONS() {
-            super(new Color(2, 253, 217), List.of(new UI().getName()));
+            super(new Color(130, 38, 248), List.of(new UI().getName()));
         }
     }
 
     public static class UI_CONSTRUCTION extends YAJLLogArea {
         public UI_CONSTRUCTION() {
-            super(new Color(0, 4, 255), List.of(new UI().getName()));
+            super(new Color(221, 0, 255), List.of(new UI().getName()));
         }
     }
 }

d090d34e39608f9795659871136a25b44683563f
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-20 16:58:45 +0200

    Added default lifecycle methods to WebSocketClientEndpoint implementation

---
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/websocket/WebSocketClientEndpoint.java
@@ -0,0 +1,24 @@
+package com.toxicstoxm.LEDSuite.communication.websocket;
+
+import com.toxicstoxm.LEDSuite.logger.LEDSuiteLogAreas;
+import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
+import jakarta.websocket.*;
+
+public abstract class WebSocketClientEndpoint {
+
+    public void onOpen(Session session) {
+        LEDSuiteApplication.getLogger().info("WebSocket connection opened with session ID: " + session.getId(), new LEDSuiteLogAreas.NETWORK());
+    }
+
+    public void onMessage(String message, Session session) {
+        LEDSuiteApplication.getLogger().info("Received message from session ID " + session.getId() + ": " + message, new LEDSuiteLogAreas.COMMUNICATION());
+    }
+
+    public void onClose(Session session) {
+        LEDSuiteApplication.getLogger().info("WebSocket connection closed with session ID: " + session.getId(), new LEDSuiteLogAreas.NETWORK());
+    }
+
+    public void onError(Session session, Throwable throwable) {
+        LEDSuiteApplication.getLogger().error("Error in WebSocket session ID " + session.getId() + ": " + throwable.getMessage(), new LEDSuiteLogAreas.NETWORK());
+    }
+}

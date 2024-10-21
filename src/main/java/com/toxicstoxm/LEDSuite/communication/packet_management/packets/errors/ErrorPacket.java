1e4bdade1e54c417dbb1c4e3f51d10175e8ed453
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-21 21:36:11 +0200

    Added ErrorPacket

---
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/errors/ErrorPacket.java
@@ -0,0 +1,73 @@
+package com.toxicstoxm.LEDSuite.communication.packet_management.packets.errors;
+
+import com.toxicstoxm.LEDSuite.Constants;
+import com.toxicstoxm.LEDSuite.communication.packet_management.CommunicationPacket;
+import com.toxicstoxm.LEDSuite.communication.packet_management.Packet;
+import com.toxicstoxm.LEDSuite.communication.packet_management.PacketManager;
+import com.toxicstoxm.LEDSuite.logger.LEDSuiteLogAreas;
+import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
+import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
+import com.toxicstoxm.YAJSI.api.yaml.InvalidConfigurationException;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ErrorPacket extends CommunicationPacket {
+
+   private String source;        // guaranteed
+   private int code;             // guaranteed
+   private String name;          // guaranteed
+   private int severity;         // guaranteed
+
+    @Override
+    public String getType() {
+        return "error";
+    }
+
+    @Override
+    public String getSubType() {
+        return "";
+    }
+
+    @Override
+    public Packet deserialize(String yamlString) throws PacketManager.DeserializationException {
+        YamlConfiguration yaml;
+        try {
+            yaml = loadYAML(yamlString);
+        } catch (InvalidConfigurationException e) {
+            throw new PacketManager.DeserializationException(e);
+        }
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.Error.SOURCE, yaml);
+        source = yaml.getString(Constants.Communication.YAML.Keys.Error.SOURCE);
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.Error.CODE, yaml);
+        code = yaml.getInt(Constants.Communication.YAML.Keys.Error.CODE);
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.Error.NAME, yaml);
+        name = yaml.getString(Constants.Communication.YAML.Keys.Error.NAME);
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.Error.SEVERITY, yaml);
+        severity = yaml.getInt(Constants.Communication.YAML.Keys.Error.SEVERITY);
+
+        return this;
+    }
+
+    @Override
+    public String serialize() {
+        YamlConfiguration yaml = saveYAML();
+
+        yaml.set(Constants.Communication.YAML.Keys.Error.SOURCE, source);
+        yaml.set(Constants.Communication.YAML.Keys.Error.CODE, code);
+        yaml.set(Constants.Communication.YAML.Keys.Error.NAME, name);
+        yaml.set(Constants.Communication.YAML.Keys.Error.SEVERITY, severity);
+
+        return yaml.saveToString();
+    }
+
+    @Override
+    public void handlePacket() {
+        LEDSuiteApplication.getLogger().info(toString(), new LEDSuiteLogAreas.COMMUNICATION());
+    }
+}

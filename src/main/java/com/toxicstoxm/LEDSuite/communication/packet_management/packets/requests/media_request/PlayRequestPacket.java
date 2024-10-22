258c743698e8415d73b24c21b2e286ffa363f892
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-22 22:23:50 +0200

    Added play request packet

---
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/media_request/PlayRequestPacket.java
@@ -0,0 +1,51 @@
+package com.toxicstoxm.LEDSuite.communication.packet_management.packets.requests.media_request;
+
+import com.toxicstoxm.LEDSuite.Constants;
+import com.toxicstoxm.LEDSuite.communication.packet_management.CommunicationPacket;
+import com.toxicstoxm.LEDSuite.communication.packet_management.Packet;
+import com.toxicstoxm.LEDSuite.communication.packet_management.PacketManager;
+import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
+import com.toxicstoxm.YAJSI.api.yaml.InvalidConfigurationException;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class PlayRequestPacket extends CommunicationPacket {
+
+    private String requestFile;
+
+    @Override
+    public String getType() {
+        return Constants.Communication.YAML.Values.PacketTypes.REQUEST;
+    }
+
+    @Override
+    public String getSubType() {
+        return Constants.Communication.YAML.Values.RequestTypes.PLAY;
+    }
+
+    @Override
+    public Packet deserialize(String yamlString) throws PacketManager.DeserializationException {
+        YamlConfiguration yaml;
+        try {
+            yaml = loadYAML(yamlString);
+        } catch (InvalidConfigurationException e) {
+            throw new PacketManager.DeserializationException(e);
+        }
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.Request.FILE, yaml);
+        requestFile = yaml.getString(Constants.Communication.YAML.Keys.Request.FILE);
+
+        return this;
+    }
+
+    @Override
+    public String serialize() {
+        YamlConfiguration yaml = saveYAML();
+
+        yaml.set(Constants.Communication.YAML.Keys.Request.FILE, requestFile);
+
+        return yaml.saveToString();
+    }
+}

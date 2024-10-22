a6e3710fabf1c9894553fc17b7c485c3576d9783
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-22 22:23:55 +0200

    Added rename request packet

---
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/RenameRequestPacket.java
@@ -0,0 +1,57 @@
+package com.toxicstoxm.LEDSuite.communication.packet_management.packets.requests;
+
+import com.toxicstoxm.LEDSuite.Constants;
+import com.toxicstoxm.LEDSuite.communication.packet_management.CommunicationPacket;
+import com.toxicstoxm.LEDSuite.communication.packet_management.Packet;
+import com.toxicstoxm.LEDSuite.communication.packet_management.PacketManager;
+import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
+import com.toxicstoxm.YAJSI.api.yaml.InvalidConfigurationException;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class RenameRequestPacket extends CommunicationPacket {
+
+    private String requestFile;
+    private String newName;
+
+    @Override
+    public String getType() {
+        return Constants.Communication.YAML.Values.PacketTypes.REQUEST;
+    }
+
+    @Override
+    public String getSubType() {
+        return Constants.Communication.YAML.Values.RequestTypes.RENAME_REQUEST;
+    }
+
+    @Override
+    public Packet deserialize(String yamlString) throws PacketManager.DeserializationException {
+        YamlConfiguration yaml;
+        try {
+            yaml = loadYAML(yamlString);
+        } catch (InvalidConfigurationException e) {
+            throw new PacketManager.DeserializationException(e);
+        }
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.Request.FILE, yaml);
+        requestFile = yaml.getString(Constants.Communication.YAML.Keys.Request.FILE);
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.RenameRequest.NEW_NAME, yaml);
+        newName = yaml.getString(Constants.Communication.YAML.Keys.RenameRequest.NEW_NAME);
+
+        return this;
+    }
+
+    @Override
+    public String serialize() {
+        YamlConfiguration yaml = saveYAML();
+
+        yaml.set(Constants.Communication.YAML.Keys.Request.FILE, requestFile);
+
+        yaml.set(Constants.Communication.YAML.Keys.RenameRequest.NEW_NAME, newName);
+
+        return yaml.saveToString();
+    }
+}

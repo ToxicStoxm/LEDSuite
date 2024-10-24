67de62c696e5d1d32015c7a9a9369b8d5f381736
dominik <dominik0815oberlechner@gmail.com>
2024-10-24 12:52:02 +0200

    Added MenuReplyPacket

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/MenuReplyPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/MenuReplyPacket.java
@@ -2,12 +2,18 @@ package com.toxicstoxm.LEDSuite.communication.packet_management.packets.replys;
 
 import com.toxicstoxm.LEDSuite.Constants;
 import com.toxicstoxm.LEDSuite.communication.packet_management.CommunicationPacket;
+import com.toxicstoxm.LEDSuite.communication.packet_management.Packet;
+import com.toxicstoxm.LEDSuite.communication.packet_management.PacketManager;
+import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
+import com.toxicstoxm.YAJSI.api.yaml.InvalidConfigurationException;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class MenuReplyPacket extends CommunicationPacket {
+
+    private String menuYAML;
 
 
     @Override
@@ -18,5 +24,26 @@ public class MenuReplyPacket extends CommunicationPacket {
     @Override
     public String getSubType() {
         return Constants.Communication.YAML.Values.ReplyTypes.MENU;
+    }
+
+    @Override
+    public String serialize() {
+        YamlConfiguration yaml = saveYAML();
+        yaml.set(Constants.Communication.YAML.Keys.MenuReply.MENU, menuYAML);
+        return yaml.saveToString();
+    }
+
+    @Override
+    public Packet deserialize(String yamlString) throws PacketManager.DeserializationException {
+        YamlConfiguration yaml;
+        try {
+            yaml = loadYAML(yamlString);
+        } catch (InvalidConfigurationException e) {
+            throw new PacketManager.DeserializationException(e);
+        }
+
+        ensureKeyExists(Constants.Communication.YAML.Keys.MenuReply.MENU, yaml);
+        menuYAML = yaml.getString(Constants.Communication.YAML.Keys.MenuReply.MENU);
+        return this;
     }
 }

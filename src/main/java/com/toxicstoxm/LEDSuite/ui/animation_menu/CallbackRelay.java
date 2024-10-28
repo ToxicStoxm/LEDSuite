2ad59d9dfbe1388837475ca8705e6c1f0d7d9cd7
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-28 22:08:08 +0100

    Removed a failed attempt to implement menu reply

--- src/main/java/com/toxicstoxm/LEDSuite/ui/animation_menu/CallbackRelay.java
+++
@@ -1,6 +0,0 @@
-package com.toxicstoxm.LEDSuite.ui.animation_menu;
-
-@FunctionalInterface
-public interface CallbackRelay {
-    void enqueueMessage(String message);
-}
--- src/main/java/com/toxicstoxm/LEDSuite/ui/animation_menu/widgets/GroupSuffixWidget.java
+++
@@ -1,4 +0,0 @@
-package com.toxicstoxm.LEDSuite.ui.animation_menu.widgets;
-
-public record GroupSuffixWidget(String label, String icon_name) {
-}

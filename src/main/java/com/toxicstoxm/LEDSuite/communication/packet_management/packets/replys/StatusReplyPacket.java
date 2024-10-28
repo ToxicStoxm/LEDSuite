ace494e5fb7e299b17692264eb76865d5c432c25
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-28 22:20:22 +0100

    fixed deserializer, structure / path rework

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/StatusReplyPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/StatusReplyPacket.java
@@ -4,8 +4,11 @@ import com.toxicstoxm.LEDSuite.Constants;
 import com.toxicstoxm.LEDSuite.communication.packet_management.CommunicationPacket;
 import com.toxicstoxm.LEDSuite.communication.packet_management.Packet;
 import com.toxicstoxm.LEDSuite.communication.packet_management.PacketManager;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.enums.FileState;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.enums.LidState;
 import com.toxicstoxm.LEDSuite.logger.LEDSuiteLogAreas;
 import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
+import com.toxicstoxm.LEDSuite.ui.dialogs.status_dialog.StatusUpdate;
 import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
 import com.toxicstoxm.YAJSI.api.yaml.ConfigurationSection;
 import com.toxicstoxm.YAJSI.api.yaml.InvalidConfigurationException;
@@ -20,31 +23,28 @@ public class StatusReplyPacket extends CommunicationPacket {
 
     public record InteractiveAnimation(String id, String label, String iconName, boolean pauseable) {}
 
-    private boolean isFileLoaded;                    // guaranteed
-    private String fileState;                        // guaranteed
-    private String selectedFile;                     // only if isFileLoaded is true
-    private double currentDraw;
-    private boolean currentDrawAvailable;            // not guaranteed
-    private double voltage;
-    private boolean voltageAvailable;                // not guaranteed
-    private boolean lidState;
-    private boolean lidStateAvailable;               // not guaranteed
-    private List<InteractiveAnimation> animations;   // only available
+    private FileState fileState;                        // guaranteed
+    private String selectedFile;
+    private Double currentDraw;                    // not guaranteed
+    private Double voltage;
+    private LidState lidState;                           // not guaranteed
+    private List<InteractiveAnimation> animations;     // only available
     private boolean animationsAvailable;
 
 
     @Override
     public String getType() {
-        return Constants.Communication.YAML.Values.PacketTypes.REPLY;
+        return Constants.Communication.YAML.Values.General.PacketTypes.REPLY;
     }
 
     @Override
     public String getSubType() {
-        return Constants.Communication.YAML.Values.ReplyTypes.STATUS;
+        return Constants.Communication.YAML.Values.Reply.Types.STATUS;
     }
 
     @Override
     public Packet deserialize(String yamlString) throws PacketManager.DeserializationException {
+        StatusReplyPacket packet = StatusReplyPacket.builder().build();
         YamlConfiguration yaml;
         try {
             yaml = loadYAML(yamlString);
@@ -52,51 +52,52 @@ public class StatusReplyPacket extends CommunicationPacket {
             throw new PacketManager.DeserializationException(e);
         }
 
-        ensureKeyExists(Constants.Communication.YAML.Keys.Status.IS_FILE_LOADED, yaml);
-        isFileLoaded = yaml.getBoolean(Constants.Communication.YAML.Keys.Status.IS_FILE_LOADED);
+        ensureKeyExists(Constants.Communication.YAML.Keys.Reply.StatusReply.FILE_STATE, yaml);
+        try {
+            packet.fileState = FileState.valueOf(yaml.getString(Constants.Communication.YAML.Keys.Reply.StatusReply.FILE_STATE));
+        } catch (IllegalArgumentException e) {
+            throw new PacketManager.DeserializationException(e);
+        }
 
-        ensureKeyExists(Constants.Communication.YAML.Keys.Status.FILE_STATE, yaml);
-        fileState = yaml.getString(Constants.Communication.YAML.Keys.Status.FILE_STATE);
+        if (!packet.fileState.equals(FileState.idle)) packet.selectedFile = yaml.getString(Constants.Communication.YAML.Keys.Reply.StatusReply.SELECTED_FILE);
+        else packet.selectedFile = "";
 
-        if (isFileLoaded) selectedFile = yaml.getString(Constants.Communication.YAML.Keys.Status.SELECTED_FILE);
-        else selectedFile = "";
+        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Reply.StatusReply.CURRENT_DRAW, yaml)) {
+            packet.currentDraw = yaml.getDouble(Constants.Communication.YAML.Keys.Reply.StatusReply.CURRENT_DRAW);
+        }
 
-        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Status.CURRENT_DRAW, yaml)) {
-            currentDraw = yaml.getDouble(Constants.Communication.YAML.Keys.Status.CURRENT_DRAW);
-        } else currentDrawAvailable = false;
+        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Reply.StatusReply.VOLTAGE, yaml)) {
+            packet.voltage = yaml.getDouble(Constants.Communication.YAML.Keys.Reply.StatusReply.VOLTAGE);
+        }
 
-        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Status.VOLTAGE, yaml)) {
-            voltage = yaml.getDouble(Constants.Communication.YAML.Keys.Status.VOLTAGE);
-        } else voltageAvailable = false;
+        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Reply.StatusReply.LID_STATE, yaml)) {
+            packet.lidState = LidState.fromBool(yaml.getBoolean(Constants.Communication.YAML.Keys.Reply.StatusReply.LID_STATE));
+        }
 
-        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Status.LID_STATE, yaml)) {
-            lidState = yaml.getBoolean(Constants.Communication.YAML.Keys.Status.LID_STATE);
-        } lidStateAvailable = false;
+        packet.animations = new ArrayList<>();
 
-        animations = new ArrayList<>();
+        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Reply.StatusReply.ANIMATIONS, yaml)) {
 
-        if (checkIfKeyExists(Constants.Communication.YAML.Keys.Status.ANIMATIONS, yaml)) {
+            ensureKeyExists(Constants.Communication.YAML.Keys.Reply.StatusReply.ANIMATIONS, yaml);
+            ConfigurationSection animationsSection = yaml.getConfigurationSection(Constants.Communication.YAML.Keys.Reply.StatusReply.ANIMATIONS);
 
-            ensureKeyExists(Constants.Communication.YAML.Keys.Status.ANIMATIONS, yaml);
-            ConfigurationSection animationsSection = yaml.getConfigurationSection(Constants.Communication.YAML.Keys.Status.ANIMATIONS);
-
-            if (animationsSection == null) throw new PacketManager.DeserializationException("Deserialization failed! ", new NullPointerException(Constants.Communication.YAML.Keys.Status.ANIMATIONS + " wasn't found!"));
+            if (animationsSection == null) throw new PacketManager.DeserializationException("Deserialization failed! ", new NullPointerException(Constants.Communication.YAML.Keys.Reply.StatusReply.ANIMATIONS + " wasn't found!"));
             for (String key : animationsSection.getKeys(false)) {
 
-                ensureKeyExists(key + "." + Constants.Communication.YAML.Keys.Status.AnimationList.LABEL, yaml);
-                ensureKeyExists(key + "." + Constants.Communication.YAML.Keys.Status.AnimationList.ICON, yaml);
-                ensureKeyExists(key + "." + Constants.Communication.YAML.Keys.Status.AnimationList.PAUSEABLE, yaml);
+                ensureKeyExists(key + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.LABEL, yaml);
+                ensureKeyExists(key + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.ICON, yaml);
+                ensureKeyExists(key + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.PAUSEABLE, yaml);
 
-                animations.add(new InteractiveAnimation(
+                packet.animations.add(new InteractiveAnimation(
                         key,
-                        animationsSection.getString(key + "." + Constants.Communication.YAML.Keys.Status.AnimationList.LABEL),
-                        animationsSection.getString(key + "." + Constants.Communication.YAML.Keys.Status.AnimationList.ICON),
-                        animationsSection.getBoolean(key + "." + Constants.Communication.YAML.Keys.Status.AnimationList.PAUSEABLE)
+                        animationsSection.getString(key + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.LABEL),
+                        animationsSection.getString(key + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.ICON),
+                        animationsSection.getBoolean(key + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.PAUSEABLE)
                 ));
             }
-        } else animationsAvailable = false;
+        } else packet.animationsAvailable = false;
 
-        return this;
+        return packet;
     }
 
     @Override
@@ -104,22 +105,21 @@ public class StatusReplyPacket extends CommunicationPacket {
         YamlConfiguration yaml = saveYAML();
 
         // Set the object's state into the YAML structure using the same keys as in deserializing
-        yaml.set(Constants.Communication.YAML.Keys.Status.IS_FILE_LOADED, isFileLoaded);
-        yaml.set(Constants.Communication.YAML.Keys.Status.FILE_STATE, fileState);
-        if (isFileLoaded) yaml.set(Constants.Communication.YAML.Keys.Status.SELECTED_FILE, selectedFile);
-        if (currentDrawAvailable) yaml.set(Constants.Communication.YAML.Keys.Status.CURRENT_DRAW, currentDraw);
-        if (voltageAvailable) yaml.set(Constants.Communication.YAML.Keys.Status.VOLTAGE, voltage);
-        if (lidStateAvailable) yaml.set(Constants.Communication.YAML.Keys.Status.LID_STATE, lidState);
+        yaml.set(Constants.Communication.YAML.Keys.Reply.StatusReply.FILE_STATE, fileState.name());
+        if (!fileState.equals(FileState.idle)) yaml.set(Constants.Communication.YAML.Keys.Reply.StatusReply.SELECTED_FILE, selectedFile);
+        if (currentDraw != null) yaml.set(Constants.Communication.YAML.Keys.Reply.StatusReply.CURRENT_DRAW, currentDraw);
+        if (voltage != null) yaml.set(Constants.Communication.YAML.Keys.Reply.StatusReply.VOLTAGE, voltage);
+        if (lidState != null) yaml.set(Constants.Communication.YAML.Keys.Reply.StatusReply.LID_STATE, lidState.asBool());
 
         // Save the list of animations
         if (animationsAvailable && animations != null && !animations.isEmpty()) {
             for (int i = 0; i < animations.size(); i++) {
                 InteractiveAnimation animation = animations.get(i);
-                String baseKey = Constants.Communication.YAML.Keys.Status.ANIMATIONS + "." + i;
+                String baseKey = Constants.Communication.YAML.Keys.Reply.StatusReply.ANIMATIONS + "." + i;
 
-                yaml.set(baseKey + "." + Constants.Communication.YAML.Keys.Status.AnimationList.LABEL, animation.label);
-                yaml.set(baseKey + "." + Constants.Communication.YAML.Keys.Status.AnimationList.ICON, animation.iconName);
-                yaml.set(baseKey + "." + Constants.Communication.YAML.Keys.Status.AnimationList.PAUSEABLE, animation.pauseable);
+                yaml.set(baseKey + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.LABEL, animation.label);
+                yaml.set(baseKey + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.ICON, animation.iconName);
+                yaml.set(baseKey + "." + Constants.Communication.YAML.Keys.Reply.StatusReply.AnimationList.PAUSEABLE, animation.pauseable);
             }
         }
 
@@ -130,6 +130,23 @@ public class StatusReplyPacket extends CommunicationPacket {
 
     @Override
     public void handlePacket() {
-        LEDSuiteApplication.getLogger().info(toString(), new LEDSuiteLogAreas.COMMUNICATION());
+
+        LEDSuiteApplication.getLogger().info(this.toString(), new LEDSuiteLogAreas.COMMUNICATION());
+
+        var statusUpdater = LEDSuiteApplication.getWindow().getStatusDialogUpdateCallback();
+
+        if (statusUpdater != null) {
+
+            statusUpdater.update(
+                    StatusUpdate.builder()
+                            .fileState(fileState)
+                            .lidState(lidState)
+                            .currentDraw(currentDraw)
+                            .voltage(voltage)
+                            .currentFile(fileState.equals(FileState.idle) ? null : selectedFile)
+                            .build()
+            );
+            LEDSuiteApplication.getLogger().info("Updated status using provided status updater!", new LEDSuiteLogAreas.COMMUNICATION());
+        } else LEDSuiteApplication.getLogger().info("Couldn't update status because no status updater is currently available!", new LEDSuiteLogAreas.COMMUNICATION());
     }
 }

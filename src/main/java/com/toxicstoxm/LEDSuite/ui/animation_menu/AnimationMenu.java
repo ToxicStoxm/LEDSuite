ca27d2ef5b96fa2a2083e5197d1508462b441b90
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-30 00:43:16 +0100

    Implemented AnimationMenu base, added some test code

--- src/main/java/com/toxicstoxm/LEDSuite/ui/animation_menu/AnimationMenu.java
+++ src/main/java/com/toxicstoxm/LEDSuite/ui/animation_menu/AnimationMenu.java
@@ -1,5 +1,6 @@
 package com.toxicstoxm.LEDSuite.ui.animation_menu;
 
+import io.github.jwharm.javagi.gtk.annotations.GtkChild;
 import io.github.jwharm.javagi.gtk.annotations.GtkTemplate;
 import io.github.jwharm.javagi.gtk.types.Types;
 import lombok.Getter;
@@ -7,8 +8,12 @@ import lombok.Setter;
 import org.gnome.adw.ActionRow;
 import org.gnome.adw.PreferencesGroup;
 import org.gnome.adw.PreferencesPage;
+import org.gnome.adw.StatusPage;
 import org.gnome.glib.Type;
 import org.gnome.gobject.GObject;
+import org.gnome.gtk.Box;
+import org.gnome.gtk.Image;
+import org.gnome.gtk.Label;
 
 import java.lang.foreign.MemorySegment;
 
@@ -21,12 +26,21 @@ import java.lang.foreign.MemorySegment;
  */
 @Getter
 @Setter
-@GtkTemplate(name = "AnimationMenu", ui = "7com/toxicstoxm/LEDSuite/AnimationMenu.ui")
-public class AnimationMenu extends PreferencesPage {
+@GtkTemplate(name = "AnimationMenu", ui = "/com/toxicstoxm/LEDSuite/AnimationMenu.ui")
+public class AnimationMenu extends Box {
 
     private String menuID;
     private String subtitle;
     private String title;
+
+    @GtkChild(name = "animation_menu_content")
+    public Box animationMenuContent;
+
+    @GtkChild(name = "animation_menu_image")
+    public Image animationMenuImage;
+
+    @GtkChild(name = "animation_menu_label")
+    public Label animationLabel;
 
     private static final Type gtype = Types.register(AnimationMenu.class);
 
@@ -44,8 +58,12 @@ public class AnimationMenu extends PreferencesPage {
 
     public AnimationMenu init() {
         var pref = PreferencesGroup.builder().setTitle("Hello World").build();
-        pref.add(ActionRow.builder().setCssClasses(new String[]{"property"}).setTitle("Hello").setSubtitle("World").build());
-        this.add(pref);
+        for (int i = 0; i < 20; i++) {
+            pref.add(ActionRow.builder().setCssClasses(new String[]{"property"}).setTitle("Hello").setSubtitleSelectable(true).setSubtitle("World").build());
+        }
+        animationLabel.setLabel("Test");
+        animationMenuImage.setFromIconName("media-optical-cd-audio-symbolic");
+        animationMenuContent.append(pref);
         return this;
     }
 

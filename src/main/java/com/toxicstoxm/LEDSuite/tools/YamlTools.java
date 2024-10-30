4666dddebecee027a3eeab554e93c3cb94cf4e9c
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-30 23:41:50 +0100

    Improved doc

--- src/main/java/com/toxicstoxm/LEDSuite/tools/YamlTools.java
+++ src/main/java/com/toxicstoxm/LEDSuite/tools/YamlTools.java
@@ -1,7 +1,6 @@
 package com.toxicstoxm.LEDSuite.tools;
 
-import com.toxicstoxm.LEDSuite.communication.DeserializationException;
-import com.toxicstoxm.LEDSuite.communication.packet_management.PacketManager;
+import com.toxicstoxm.LEDSuite.communication.packet_management.DeserializationException;
 import com.toxicstoxm.YAJSI.api.yaml.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,10 +21,11 @@ public class YamlTools {
     }
 
     /**
-     * Checks if the given key exists in the given config section. If not this throws a {@link com.toxicstoxm.LEDSuite.communication.DeserializationException}.
+     * Checks if the given key exists in the given config section.
+     * If not this throws a {@link DeserializationException}.
      * @param key the key to check for
      * @param yaml the config section to check
-     * @throws com.toxicstoxm.LEDSuite.communication.DeserializationException if the given key does not exist in the given config section
+     * @throws DeserializationException if the given key does not exist in the given config section
      */
     public static void ensureKeyExists(String key, @NotNull ConfigurationSection yaml) throws DeserializationException {
         if (!yaml.contains(key)) throw new DeserializationException("Deserialization failed! Required value " + key + " is missing!");

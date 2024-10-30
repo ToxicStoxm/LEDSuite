ab317da757b98db795080a7cfc0ca4d8ed89b479
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-30 23:43:46 +0100

    Updated imports

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/PacketReceivedHandler.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/PacketReceivedHandler.java
@@ -1,5 +1,7 @@
 package com.toxicstoxm.LEDSuite.communication.packet_management;
 
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.CommunicationPacket;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.Packet;
 import com.toxicstoxm.LEDSuite.logger.LEDSuiteLogAreas;
 import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
 import org.jetbrains.annotations.NotNull;

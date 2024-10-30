cae6c54d04ebd381b3a398d34e2b3d18be2f592e
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-30 18:38:53 +0100

    Moved DeserializationException from PacketManager to a separate file

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/RenameRequestPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/RenameRequestPacket.java
@@ -1,10 +1,10 @@
 package com.toxicstoxm.LEDSuite.communication.packet_management.packets.requests;
 
 import com.toxicstoxm.LEDSuite.Constants;
+import com.toxicstoxm.LEDSuite.communication.DeserializationException;
 import com.toxicstoxm.LEDSuite.communication.packet_management.AutoRegisterPacket;
 import com.toxicstoxm.LEDSuite.communication.packet_management.CommunicationPacket;
 import com.toxicstoxm.LEDSuite.communication.packet_management.Packet;
-import com.toxicstoxm.LEDSuite.communication.packet_management.PacketManager;
 import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
 import com.toxicstoxm.YAJSI.api.yaml.InvalidConfigurationException;
 import lombok.*;
@@ -36,17 +36,17 @@ public class RenameRequestPacket extends CommunicationPacket {
     }
 
     @Override
-    public Packet deserialize(String yamlString) throws PacketManager.DeserializationException {
+    public Packet deserialize(String yamlString) throws DeserializationException {
         RenameRequestPacket packet = RenameRequestPacket.builder().build();
         YamlConfiguration yaml;
         try {
             yaml = loadYAML(yamlString);
         } catch (InvalidConfigurationException e) {
-            throw new PacketManager.DeserializationException(e);
+            throw new DeserializationException(e);
         }
 
-        ensureKeyExists(Constants.Communication.YAML.Keys.Request.General.FILE, yaml);
-        packet.requestFile = yaml.getString(Constants.Communication.YAML.Keys.Request.General.FILE);
+        ensureKeyExists(Constants.Communication.YAML.Keys.Request.General.FILE_NAME, yaml);
+        packet.requestFile = yaml.getString(Constants.Communication.YAML.Keys.Request.General.FILE_NAME);
 
         ensureKeyExists(Constants.Communication.YAML.Keys.Request.RenameRequest.NEW_NAME, yaml);
         packet.newName = yaml.getString(Constants.Communication.YAML.Keys.Request.RenameRequest.NEW_NAME);
@@ -58,7 +58,7 @@ public class RenameRequestPacket extends CommunicationPacket {
     public String serialize() {
         YamlConfiguration yaml = saveYAML();
 
-        yaml.set(Constants.Communication.YAML.Keys.Request.General.FILE, requestFile);
+        yaml.set(Constants.Communication.YAML.Keys.Request.General.FILE_NAME, requestFile);
 
         yaml.set(Constants.Communication.YAML.Keys.Request.RenameRequest.NEW_NAME, newName);
 

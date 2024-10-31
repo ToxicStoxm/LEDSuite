4993178e5410dab661e43c02035a9d25f5d5e978
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-10-31 02:54:37 +0100

    Add widget type enum

---
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/animation_menu/WidgetType.java
@@ -0,0 +1,20 @@
+package com.toxicstoxm.LEDSuite.communication.packet_management.animation_menu;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum WidgetType {
+    GROUP,
+    BUTTON_ROW,
+    BUTTON,
+    ENTRY_ROW,
+    PROPERTY_ROW,
+    COMBO_ROW,
+    SWITCH_ROW,
+    SPIN_ROW,
+    EXPANDER_ROW;
+
+    @NotNull String getName() {
+        return this.name().toLowerCase();
+    }
+
+}

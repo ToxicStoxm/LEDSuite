d5f2bb1640fc3a7cf6491c0fcf95f4d6157e8699
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-01 20:34:13 +0100

    Added connectivity status interface to settings dialog

---
+++ src/main/java/com/toxicstoxm/LEDSuite/ui/dialogs/settings_dialog/ConnectivityStatus.java
@@ -0,0 +1,8 @@
+package com.toxicstoxm.LEDSuite.ui.dialogs.settings_dialog;
+
+public interface ConnectivityStatus {
+    void connected();
+    void disconnected();
+    void disconnecting();
+    void connecting();
+}

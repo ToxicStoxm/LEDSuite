78c5a664c62886999787db12d45a5fe71cdfddef
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-02 02:42:28 +0100

    Implemented menu reply handling

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/MenuReplyPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/MenuReplyPacket.java
@@ -1,12 +1,18 @@
 package com.toxicstoxm.LEDSuite.communication.packet_management.packets.replys;
 
 import com.toxicstoxm.LEDSuite.Constants;
+import com.toxicstoxm.LEDSuite.auto_registration.AutoRegister;
 import com.toxicstoxm.LEDSuite.auto_registration.modules.AutoRegisterModules;
 import com.toxicstoxm.LEDSuite.communication.packet_management.DeserializationException;
-import com.toxicstoxm.LEDSuite.auto_registration.AutoRegister;
 import com.toxicstoxm.LEDSuite.communication.packet_management.packets.CommunicationPacket;
 import com.toxicstoxm.LEDSuite.communication.packet_management.packets.Packet;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.errors.menu_error.Code;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.errors.menu_error.MenuErrorPacket;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.errors.menu_error.Severity;
 import com.toxicstoxm.LEDSuite.communication.packet_management.packets.requests.MenuRequestPacket;
+import com.toxicstoxm.LEDSuite.logger.LEDSuiteLogAreas;
+import com.toxicstoxm.LEDSuite.tools.YamlTools;
+import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
 import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
 import com.toxicstoxm.YAJSI.api.yaml.InvalidConfigurationException;
 import lombok.*;
@@ -54,12 +60,45 @@ public class MenuReplyPacket extends CommunicationPacket {
             throw new DeserializationException(e);
         }
 
-        ensureKeyExists(Constants.Communication.YAML.Keys.Reply.MenuReply.CONTENT, yaml);
+        //ensureKeyExists(Constants.Communication.YAML.Keys.Reply.MenuReply.CONTENT, yaml);
         packet.menuYAML = yaml.getString(Constants.Communication.YAML.Keys.Reply.MenuReply.CONTENT);
         return packet;
     }
 
     @Override
     public void handlePacket() {
+
+        var animationMenuManager = LEDSuiteApplication.getAnimationMenuManager();
+        if (animationMenuManager != null) {
+            try {
+                LEDSuiteApplication.getWindow().displayAnimationManu(animationMenuManager.deserializeAnimationMenu(menuYAML));
+            } catch (DeserializationException e) {
+                LEDSuiteApplication.getLogger().warn("Failed to handle menu reply! Deserialization failed: " + e.getMessage());
+                e.printStackTrace();
+
+
+                String fileName = null;
+
+
+                try {
+                    YamlConfiguration yaml = new YamlConfiguration();
+                    yaml.loadFromString(menuYAML);
+                    if (YamlTools.checkIfKeyExists(Constants.Communication.YAML.Keys.Reply.MenuReply.FILENAME, yaml)) {
+                        fileName = yaml.getString(Constants.Communication.YAML.Keys.Reply.MenuReply.FILENAME);
+                    }
+                } catch (InvalidConfigurationException ex) {
+                    LEDSuiteApplication.getLogger().warn("Failed to get file name for error reporting menu deserialization failure!", new LEDSuiteLogAreas.COMMUNICATION());
+                }
+
+                LEDSuiteApplication.getWebSocketCommunication().enqueueMessage(
+                        MenuErrorPacket.builder()
+                                .code(Code.PARSE_ERROR)
+                                .fileName(fileName)
+                                .severity(Severity.FATAL)
+                                .message(e.getMessage())
+                                .build().serialize()
+                );
+            }
+        } else LEDSuiteApplication.getLogger().warn("Couldn't handle menu reply packet because animation menu manager is not available!", new LEDSuiteLogAreas.COMMUNICATION());
     }
 }

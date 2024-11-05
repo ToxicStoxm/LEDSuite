7c51091bb0979f50f7703bb5be4e132bf71894ed
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-05 22:33:36 +0100

    Renamed 'checksum' to 'sha256' and added more error sources

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/FileUploadRequestPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/FileUploadRequestPacket.java
@@ -29,7 +29,7 @@ public class FileUploadRequestPacket extends CommunicationPacket {
 
     private String requestFile;
     private String uploadSessionId;
-    private String checksum;
+    private String sha256;
 
     @Override
     public String getType() {
@@ -57,8 +57,8 @@ public class FileUploadRequestPacket extends CommunicationPacket {
         ensureKeyExists(Constants.Communication.YAML.Keys.Request.FileUploadRequest.UPLOAD_SESSION_ID, yaml);
         packet.uploadSessionId = yaml.getString(Constants.Communication.YAML.Keys.Request.FileUploadRequest.UPLOAD_SESSION_ID);
 
-        ensureKeyExists(Constants.Communication.YAML.Keys.Request.FileUploadRequest.CHECKSUM, yaml);
-        packet.checksum = yaml.getString(Constants.Communication.YAML.Keys.Request.FileUploadRequest.CHECKSUM);
+        ensureKeyExists(Constants.Communication.YAML.Keys.Request.FileUploadRequest.SHA256, yaml);
+        packet.sha256 = yaml.getString(Constants.Communication.YAML.Keys.Request.FileUploadRequest.SHA256);
 
         return packet;
     }
@@ -69,7 +69,7 @@ public class FileUploadRequestPacket extends CommunicationPacket {
 
         yaml.set(Constants.Communication.YAML.Keys.Request.General.FILE_NAME, requestFile);
         yaml.set(Constants.Communication.YAML.Keys.Request.FileUploadRequest.UPLOAD_SESSION_ID, uploadSessionId);
-        yaml.set(Constants.Communication.YAML.Keys.Request.FileUploadRequest.CHECKSUM, checksum);
+        yaml.set(Constants.Communication.YAML.Keys.Request.FileUploadRequest.SHA256, sha256);
 
         return yaml.saveToString();
     }

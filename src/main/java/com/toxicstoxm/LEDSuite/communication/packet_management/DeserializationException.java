9aec2aac847d5d6d21e16bfb9917a7124ca0a9f0
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-10 02:44:33 +0100

    DeserializationException now has an ErrorCode property. This allows for error escalation

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/DeserializationException.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/DeserializationException.java
@@ -1,27 +1,58 @@
 package com.toxicstoxm.LEDSuite.communication.packet_management;
 
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.errors.ErrorCode;
+import lombok.Getter;
+
 /**
  * {@code DeserializationException} is an unchecked exception and is used to express an error during deserialization of a communication packet.
  * @since 1.0.0
  */
+@Getter
 public class DeserializationException extends RuntimeException {
+
+    private ErrorCode errorCode = null;
+
     public DeserializationException() {
         super();
+    }
+
+    public DeserializationException(ErrorCode errorCode) {
+        super();
+        this.errorCode = errorCode;
     }
 
     public DeserializationException(String message) {
         super(message);
     }
 
+    public DeserializationException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
     public DeserializationException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public DeserializationException(String message, Throwable cause, ErrorCode errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
     }
 
     public DeserializationException(Throwable cause) {
         super(cause);
     }
 
+    public DeserializationException(Throwable cause, ErrorCode errorCode) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+
     protected DeserializationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+    protected DeserializationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ErrorCode errorCode) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.errorCode = errorCode;
     }
 }

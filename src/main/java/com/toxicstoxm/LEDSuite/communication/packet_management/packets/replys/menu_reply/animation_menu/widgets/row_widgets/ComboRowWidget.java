bbeccce48daf8247997cb8aa4becfc723c4d004a
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-10 02:52:50 +0100

    Improved error handling

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/menu_reply/animation_menu/widgets/row_widgets/ComboRowWidget.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/menu_reply/animation_menu/widgets/row_widgets/ComboRowWidget.java
@@ -1,12 +1,13 @@
-package com.toxicstoxm.LEDSuite.communication.packet_management.animation_menu.widgets.row_widgets;
+package com.toxicstoxm.LEDSuite.communication.packet_management.packets.replys.menu_reply.animation_menu.widgets.row_widgets;
 
 import com.toxicstoxm.LEDSuite.Constants;
 import com.toxicstoxm.LEDSuite.auto_registration.AutoRegister;
 import com.toxicstoxm.LEDSuite.auto_registration.modules.AutoRegisterModules;
 import com.toxicstoxm.LEDSuite.communication.packet_management.DeserializationException;
-import com.toxicstoxm.LEDSuite.communication.packet_management.animation_menu.DeserializableWidget;
-import com.toxicstoxm.LEDSuite.communication.packet_management.animation_menu.WidgetType;
-import com.toxicstoxm.LEDSuite.communication.packet_management.animation_menu.widgets.templates.AnimationMenuActionRowWidget;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.errors.ErrorCode;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.replys.menu_reply.animation_menu.DeserializableWidget;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.replys.menu_reply.animation_menu.WidgetType;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.replys.menu_reply.animation_menu.widgets.templates.AnimationMenuActionRowWidget;
 import org.gnome.adw.ComboRow;
 import org.gnome.glib.Type;
 import org.gnome.gtk.Gtk;
@@ -38,7 +39,7 @@ public class ComboRowWidget extends AnimationMenuActionRowWidget<ComboRow> {
         ensureKeyExists(Constants.Communication.YAML.Keys.Reply.MenuReply.CONTENT);
         List<String> content = widgetSection.getStringList(Constants.Communication.YAML.Keys.Reply.MenuReply.CONTENT);
 
-        if (content.isEmpty()) throw new DeserializationException("Combo row without content is disallowed!");
+        if (content.isEmpty()) throw new DeserializationException("Combo row without content is disallowed!", ErrorCode.ComboRowWithoutContent);
 
         widget.setModel(
                 StringList.builder().setStrings(content.toArray(new String[]{})).build()

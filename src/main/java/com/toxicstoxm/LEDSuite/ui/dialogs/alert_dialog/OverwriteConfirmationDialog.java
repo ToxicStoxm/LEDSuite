730599a816d2a520ff7971114eb68f999326210a
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-11 22:49:00 +0100

    Added OverwriteConfirmationDialog

---
+++ src/main/java/com/toxicstoxm/LEDSuite/ui/dialogs/alert_dialog/OverwriteConfirmationDialog.java
@@ -0,0 +1,39 @@
+package com.toxicstoxm.LEDSuite.ui.dialogs.alert_dialog;
+
+import io.github.jwharm.javagi.gtk.annotations.GtkTemplate;
+import io.github.jwharm.javagi.gtk.types.TemplateTypes;
+import org.gnome.adw.AlertDialog;
+import org.gnome.glib.Type;
+import org.gnome.gobject.GObject;
+
+import java.lang.foreign.MemorySegment;
+
+@GtkTemplate(name = "OverwriteConfirmationDialog", ui = "/com/toxicstoxm/LEDSuite/OverwriteConfirmationDialog.ui")
+public class OverwriteConfirmationDialog extends AlertDialog {
+    private static final Type gtype = TemplateTypes.register(OverwriteConfirmationDialog.class);
+
+    public OverwriteConfirmationDialog(MemorySegment address) {
+        super(address);
+    }
+
+    public static Type getType() {
+        return gtype;
+    }
+
+    public static OverwriteConfirmationDialog create() {
+        return GObject.newInstance(getType());
+    }
+
+    private ResponseCallback responseCallback;
+
+    public void onResponse(ResponseCallback responseCallback) {
+        this.responseCallback = responseCallback;
+    }
+
+    @Override
+    protected void response(String response) {
+        if (responseCallback != null) {
+            responseCallback.run(response);
+        }
+    }
+}

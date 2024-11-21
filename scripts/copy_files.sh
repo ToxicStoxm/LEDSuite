e2df69759a1c63b4655afe518eb1330b306758d6
Hannes Campidell <hannes@crafti-servi.com>
2024-11-22 00:10:36 +0100

    Added rudementary flatpak support

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -3,3 +3,10 @@
 cp $MESON_SOURCE_ROOT/build.gradle $MESON_BUILD_ROOT
 cp $MESON_SOURCE_ROOT/settings.gradle $MESON_BUILD_ROOT
 cp -r $MESON_SOURCE_ROOT/src $MESON_BUILD_ROOT
+
+OFFLINE_REPO_SOURCE=$MESON_SOURCE_ROOT/offline-repository
+
+if test -d "$OFFLINE_REPO_SOURCE"; then
+	echo "Found offline repo for flatpak build, moving it to build directory"
+	cp -r $OFFLINE_REPO_SOURCE $MESON_BUILD_ROOT
+fi

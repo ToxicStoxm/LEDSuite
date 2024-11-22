d1043e26200db375da3775ef88d6975a3915db8b
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-22 22:32:14 +0100

    Modified AuthStatus

--- src/main/java/com/toxicstoxm/LEDSuite/ui/dialogs/settings_dialog/AuthStatus.java
+++ src/main/java/com/toxicstoxm/LEDSuite/ui/dialogs/settings_dialog/AuthStatus.java
@@ -2,5 +2,8 @@ package com.toxicstoxm.LEDSuite.ui.dialogs.settings_dialog;
 
 public interface AuthStatus {
     void setAuthenticating();
-    void setAuthenticated(boolean authenticated);
+    void setAuthenticated(boolean authenticated, String username);
+    default void setAuthenticated(boolean authenticated) {
+        setAuthenticated(authenticated, null);
+    }
 }

fe189652981e55db4d3f5b16e572289f71228dca
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-23 01:42:18 +0100

    Added docs to MenuReplyPacket

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/menu_reply/MenuReplyPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/replys/menu_reply/MenuReplyPacket.java
@@ -24,10 +24,13 @@ import org.gnome.glib.GLib;
 import org.gnome.gtk.Align;
 
 /**
- * <strong>Meaning:</strong><br>
- * Structure and contents of an animation menu.
+ * <p>Represents a reply packet containing an animation menu. This packet is typically sent in response to a menu request,
+ * and it contains a serialized representation of the menu, typically in YAML format.</p>
+ * <p>This packet is used to handle the deserialization and display of animation menus in the user interface.</p>
+ *
  * @since 1.0.0
  * @see MenuRequestPacket
+ * @see MenuErrorPacket
  */
 @AllArgsConstructor
 @AutoRegister(module = AutoRegisterModules.PACKETS)
@@ -37,18 +40,37 @@ import org.gnome.gtk.Align;
 @Setter
 public class MenuReplyPacket extends CommunicationPacket {
 
+    /**
+     * A serialized YAML string representing the animation menu.
+     * This YAML content defines the structure and contents of the menu that should be displayed.
+     */
     private String menuYAML;
 
+    /**
+     * Returns the type of the packet as defined in the communication protocol.
+     *
+     * @return the type of the packet, which is {@link Constants.Communication.YAML.Values.General.PacketTypes#REPLY}.
+     */
     @Override
     public String getType() {
         return Constants.Communication.YAML.Values.General.PacketTypes.REPLY;
     }
 
+    /**
+     * Returns the subtype of the packet, which indicates that this is a menu reply packet.
+     *
+     * @return the subtype of the packet, which is {@link Constants.Communication.YAML.Values.Reply.Types#MENU}.
+     */
     @Override
     public String getSubType() {
         return Constants.Communication.YAML.Values.Reply.Types.MENU;
     }
 
+    /**
+     * Serializes the contents of this packet into a YAML string.
+     *
+     * @return a YAML string representing the contents of this {@link MenuReplyPacket}.
+     */
     @Override
     public String serialize() {
         super.serialize();
@@ -56,20 +78,31 @@ public class MenuReplyPacket extends CommunicationPacket {
         return yaml.saveToString();
     }
 
+    /**
+     * Deserializes the given YAML string into a {@link MenuReplyPacket}.
+     *
+     * @param yamlString the serialized YAML string representing the menu.
+     * @return the deserialized {@link MenuReplyPacket}.
+     * @throws DeserializationException if there is an error during deserialization.
+     */
     @Override
     public Packet deserialize(String yamlString) throws DeserializationException {
         MenuReplyPacket packet = MenuReplyPacket.builder().build();
-
         packet.menuYAML = yamlString;
         return packet;
     }
 
     private static boolean lock = false;
 
+    /**
+     * Handles the packet by deserializing the animation menu and displaying it in the user interface.
+     * <p>If another menu reply is already being processed, the packet will be ignored to prevent concurrency issues.</p>
+     * <p>If an error occurs during deserialization or while displaying the menu, an error report is sent back to the server.</p>
+     */
     @Override
     public void handlePacket() {
         if (lock) {
-            LEDSuiteApplication.getLogger().warn("Voiding menu reply because other one is currently being handled!", new LEDSuiteLogAreas.COMMUNICATION());
+            LEDSuiteApplication.getLogger().warn("Voiding menu reply because another one is currently being handled!", new LEDSuiteLogAreas.COMMUNICATION());
             return;
         }
         lock = true;
@@ -85,7 +118,6 @@ public class MenuReplyPacket extends CommunicationPacket {
                         .setHexpand(true)
                         .setTighteningThreshold(50)
                         .build()
-
         );
 
         new LEDSuiteRunnable() {

c77c1e472f41b0502e20d59af18873b25d5d33d5
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-23 01:44:00 +0100

    Added docs to RenameRequestPacket

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/RenameRequestPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/RenameRequestPacket.java
@@ -9,8 +9,26 @@ import com.toxicstoxm.LEDSuite.communication.packet_management.packets.Packet;
 import lombok.*;
 
 /**
- * <strong>Meaning:</strong><br>
- * Request for renaming the specified file to the specified name.
+ * Represents a request to rename a file.
+ * <p>
+ * This packet is used to request the renaming of a specific file, identified by {@code requestFile},
+ * to a new name specified in {@code newName}.
+ * </p>
+ *
+ * <h3>Serialization and Deserialization:</h3>
+ * <ul>
+ *     <li><strong>serialize:</strong> Converts this packet into a YAML string.</li>
+ *     <li><strong>deserialize:</strong> Populates this packet from a YAML string.</li>
+ * </ul>
+ *
+ * <h3>Usage Example:</h3>
+ * <pre>
+ * RenameRequestPacket packet = RenameRequestPacket.builder()
+ *         .requestFile("old_name.jar")
+ *         .newName("new_name.jar")
+ *         .build();
+ * </pre>
+ *
  * @since 1.0.0
  */
 @AllArgsConstructor
@@ -21,19 +39,52 @@ import lombok.*;
 @Setter
 public class RenameRequestPacket extends CommunicationPacket {
 
+    /**
+     * The name of the file to be renamed.
+     */
     private String requestFile;
+
+    /**
+     * The new name for the specified file.
+     */
     private String newName;
 
+    /**
+     * Returns the type of this packet.
+     * <p>
+     * This method identifies the packet type as a {@code REQUEST}.
+     * </p>
+     *
+     * @return the packet type string.
+     */
     @Override
     public String getType() {
         return Constants.Communication.YAML.Values.General.PacketTypes.REQUEST;
     }
 
+    /**
+     * Returns the subtype of this packet.
+     * <p>
+     * This method identifies the specific purpose of the packet as a {@code RENAME_REQUEST}.
+     * </p>
+     *
+     * @return the packet subtype string.
+     */
     @Override
     public String getSubType() {
         return Constants.Communication.YAML.Values.Request.Types.RENAME_REQUEST;
     }
 
+    /**
+     * Deserializes a YAML string into a {@code RenameRequestPacket}.
+     * <p>
+     * Populates the {@code requestFile} and {@code newName} fields based on their respective YAML keys.
+     * </p>
+     *
+     * @param yamlString the YAML string to deserialize.
+     * @return a populated {@code RenameRequestPacket}.
+     * @throws DeserializationException if the required keys are missing or invalid.
+     */
     @Override
     public Packet deserialize(String yamlString) throws DeserializationException {
         super.deserialize(yamlString);
@@ -48,6 +99,14 @@ public class RenameRequestPacket extends CommunicationPacket {
         return packet;
     }
 
+    /**
+     * Serializes this {@code RenameRequestPacket} into a YAML string.
+     * <p>
+     * Converts the {@code requestFile} and {@code newName} fields into YAML key-value pairs.
+     * </p>
+     *
+     * @return the serialized YAML string representation of this packet.
+     */
     @Override
     public String serialize() {
         super.serialize();

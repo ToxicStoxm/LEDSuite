e6c1712173ee7231745b2b655103798563a8d369
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-23 01:49:23 +0100

    Added docs to UploadStatistics

--- src/main/java/com/toxicstoxm/LEDSuite/ui/UploadStatistics.java
+++ src/main/java/com/toxicstoxm/LEDSuite/ui/UploadStatistics.java
@@ -5,9 +5,13 @@ import lombok.Builder;
 /**
  * Wrapper for upload statistics.
  * Used by the animation upload implementation to display upload statistics in {@link UploadPage}.
+ *
  * @since 1.0.0
- * @param bytesPerSecond
- * @param millisecondsRemaining
+ * @param bytesPerSecond Upload speed in bytes per second.
+ * @param millisecondsRemaining Time remaining for the upload to complete, in milliseconds.
  */
 @Builder
-public record UploadStatistics(long bytesPerSecond, long millisecondsRemaining) {}
+public record UploadStatistics(
+        long bytesPerSecond,
+        long millisecondsRemaining
+) {}

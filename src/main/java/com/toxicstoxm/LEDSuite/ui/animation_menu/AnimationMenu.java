6b87c1513f305de4186b1527f4cb5c0824bc82ee
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-23 01:34:10 +0100

    Add docs to AnimationMenu

--- src/main/java/com/toxicstoxm/LEDSuite/ui/animation_menu/AnimationMenu.java
+++ src/main/java/com/toxicstoxm/LEDSuite/ui/animation_menu/AnimationMenu.java
@@ -16,11 +16,14 @@ import org.jetbrains.annotations.NotNull;
 
 import java.lang.foreign.MemorySegment;
 
-
 /**
- * Animation menu template class.
- * Represents an animation settings menu.
- * <br>Template file: {@code AnimationMenu.ui}
+ * Represents the settings menu for animations within the application.
+ * <p>
+ * This class defines a template for the animation menu and provides methods
+ * to initialize and configure the components within the menu. The UI is defined
+ * in the {@code AnimationMenu.ui} template file.
+ * </p>
+ *
  * @since 1.0.0
  */
 @Getter
@@ -28,8 +31,19 @@ import java.lang.foreign.MemorySegment;
 @GtkTemplate(name = "AnimationMenu", ui = "/com/toxicstoxm/LEDSuite/AnimationMenu.ui")
 public class AnimationMenu extends Box {
 
+    /**
+     * Unique identifier for the menu instance.
+     */
     private String menuID;
+
+    /**
+     * Subtitle text for the menu.
+     */
     private String subtitle;
+
+    /**
+     * Title text for the menu.
+     */
     private String title;
 
     @GtkChild(name = "animation_menu_content")
@@ -44,27 +58,61 @@ public class AnimationMenu extends Box {
     @GtkChild(name = "animation_menu_subtitle")
     public Label animationSubtitle;
 
+    // The GTK type for this class, used for type registration
     private static final Type gtype = TemplateTypes.register(AnimationMenu.class);
 
+    /**
+     * Constructor that initializes the AnimationMenu using a memory address segment.
+     *
+     * @param address a {@link MemorySegment} representing the address to initialize the widget
+     */
     public AnimationMenu(MemorySegment address) {
         super(address);
     }
 
+    /**
+     * Retrieves the GTK type for this widget.
+     *
+     * @return the type of the AnimationMenu widget
+     */
     public static Type getType() {
         return gtype;
     }
 
+    /**
+     * Creates a new instance of the {@code AnimationMenu} with the specified file name.
+     * This method also sets the {@code menuID} to the provided file name.
+     *
+     * @param fileName the name of the file to associate with the menu instance
+     * @return a newly created {@code AnimationMenu} instance
+     */
     public static @NotNull AnimationMenu create(String fileName) {
         AnimationMenu menu = GObject.newInstance(getType());
         menu.setMenuID(fileName);
         return menu;
     }
 
+    /**
+     * Initializes the menu with the provided animation row data.
+     * <p>
+     * This method sets the menu label, subtitle, and image according to the properties
+     * in the provided {@link AnimationRow}.
+     * </p>
+     *
+     * @param row the {@link AnimationRow} containing data for initializing the menu
+     * @return the initialized {@code AnimationMenu} instance
+     */
     public AnimationMenu init(@NotNull AnimationRow row) {
+        // Set the label of the animation menu
         animationLabel.setLabel(row.animationRowLabel.getLabel());
+
+        // Determine the image storage type and set the image accordingly
         var storageType = row.animationIcon.getStorageType();
-        if (storageType.equals(ImageType.EMPTY) || storageType.equals(ImageType.PAINTABLE)) animationMenuImage.setFromPaintable(row.animationIcon.getPaintable());
-        else animationMenuImage.setFromIconName(row.animationIcon.getIconName());
+        if (storageType.equals(ImageType.EMPTY) || storageType.equals(ImageType.PAINTABLE)) {
+            animationMenuImage.setFromPaintable(row.animationIcon.getPaintable());
+        } else {
+            animationMenuImage.setFromIconName(row.animationIcon.getIconName());
+        }
         return this;
     }
 

ac040c14da6dee1c7bd3e060e1665282fcbfaf85
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-23 01:42:23 +0100

    Added docs to MenuRequestPacket

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/MenuRequestPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/MenuRequestPacket.java
@@ -10,8 +10,31 @@ import com.toxicstoxm.LEDSuite.communication.packet_management.packets.replys.me
 import lombok.*;
 
 /**
- * <strong>Meaning:</strong><br>
- * Request for getting the specified animations menu.
+ * Represents a request to retrieve the menu for a specific animation file.
+ * <p>
+ * This packet is used to request the server to provide the menu associated with
+ * a specified animation, identified by {@code requestFile}.
+ * </p>
+ *
+ * <h3>Key Features:</h3>
+ * <ul>
+ *     <li>Specifies the animation file whose menu is being requested.</li>
+ *     <li>Used in conjunction with a {@link MenuReplyPacket} to retrieve the menu data.</li>
+ * </ul>
+ *
+ * <h3>Serialization and Deserialization:</h3>
+ * <ul>
+ *     <li><strong>serialize:</strong> Converts this packet into a YAML string.</li>
+ *     <li><strong>deserialize:</strong> Populates this packet from a YAML string.</li>
+ * </ul>
+ *
+ * <h3>Usage Example:</h3>
+ * <pre>
+ * MenuRequestPacket packet = MenuRequestPacket.builder()
+ *         .requestFile("example_animation.led")
+ *         .build();
+ * </pre>
+ *
  * @since 1.0.0
  * @see MenuReplyPacket
  */
@@ -23,18 +46,50 @@ import lombok.*;
 @Setter
 public class MenuRequestPacket extends CommunicationPacket {
 
+    /**
+     * The name of the animation file for which the menu is being requested.
+     * <p>
+     * This file name helps the server identify which animation's menu to retrieve.
+     * </p>
+     */
     private String requestFile;
 
+    /**
+     * Returns the type of this packet.
+     * <p>
+     * This method identifies the packet type as a {@code REQUEST}.
+     * </p>
+     *
+     * @return the packet type string.
+     */
     @Override
     public String getType() {
         return Constants.Communication.YAML.Values.General.PacketTypes.REQUEST;
     }
 
+    /**
+     * Returns the subtype of this packet.
+     * <p>
+     * This method identifies the specific purpose of the packet as a {@code MENU} request.
+     * </p>
+     *
+     * @return the packet subtype string.
+     */
     @Override
     public String getSubType() {
         return Constants.Communication.YAML.Values.Request.Types.MENU;
     }
 
+    /**
+     * Deserializes a YAML string into a {@code MenuRequestPacket}.
+     * <p>
+     * Populates the {@code requestFile} field based on the corresponding YAML key.
+     * </p>
+     *
+     * @param yamlString the YAML string to deserialize.
+     * @return a populated {@code MenuRequestPacket}.
+     * @throws DeserializationException if the required key is missing or invalid.
+     */
     @Override
     public Packet deserialize(String yamlString) throws DeserializationException {
         super.deserialize(yamlString);
@@ -46,6 +101,14 @@ public class MenuRequestPacket extends CommunicationPacket {
         return packet;
     }
 
+    /**
+     * Serializes this {@code MenuRequestPacket} into a YAML string.
+     * <p>
+     * Converts the {@code requestFile} field into a YAML key-value pair.
+     * </p>
+     *
+     * @return the serialized YAML string representation of this packet.
+     */
     @Override
     public String serialize() {
         super.serialize();
@@ -54,5 +117,4 @@ public class MenuRequestPacket extends CommunicationPacket {
 
         return yaml.saveToString();
     }
-
 }

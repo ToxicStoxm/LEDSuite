85f295b5909796f5ecf26ed694a0b7a2bb824c06
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-23 01:43:04 +0100

    Added docs to PlayRequestPacket

--- src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/media_request/PlayRequestPacket.java
+++ src/main/java/com/toxicstoxm/LEDSuite/communication/packet_management/packets/requests/media_request/PlayRequestPacket.java
@@ -6,8 +6,31 @@ import com.toxicstoxm.LEDSuite.auto_registration.modules.AutoRegisterModules;
 import lombok.*;
 
 /**
- * <strong>Meaning:</strong><br>
- * Request for starting the specified animation.
+ * Represents a request to start playing the specified animation.
+ * <p>
+ * This packet is part of the media control system and is used to instruct the server
+ * to begin playback of an animation associated with a specified file.
+ * </p>
+ *
+ * <h3>Key Features:</h3>
+ * <ul>
+ *     <li>Supports automatic registration via the {@link AutoRegister} annotation.</li>
+ *     <li>Implements the {@link MediaRequestPacket} abstract class, inheriting common media request functionality.</li>
+ *     <li>Defines a specific subtype, {@code PLAY}, which corresponds to the play action in the communication protocol.</li>
+ * </ul>
+ *
+ * <h3>Usage:</h3>
+ * <p>
+ * To create a play request, instantiate this class and set the desired file using the {@code requestFile} property.
+ * Example:
+ * </p>
+ * <pre>
+ *     PlayRequestPacket packet = PlayRequestPacket.builder()
+ *             .requestFile("animation_file_name")
+ *             .build();
+ * </pre>
+ *
+ * @see MediaRequestPacket
  * @since 1.0.0
  */
 @AllArgsConstructor
@@ -18,8 +41,23 @@ import lombok.*;
 @Setter
 public class PlayRequestPacket extends MediaRequestPacket {
 
+    /**
+     * The file associated with this play request.
+     * <p>
+     * This field specifies the animation file that is to be played. The server uses this
+     * information to identify and begin playback of the animation.
+     * </p>
+     */
     private String requestFile;
 
+    /**
+     * Returns the sub-type of this media request packet.
+     * <p>
+     * This method identifies the specific action of this packet as {@code PLAY}.
+     * </p>
+     *
+     * @return a string representing the sub-type of this request packet.
+     */
     @Override
     public String getSubType() {
         return Constants.Communication.YAML.Values.Request.Types.PLAY;

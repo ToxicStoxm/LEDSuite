da66539738f807bc7040b29d114e3dc9e78ab8c9
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-11-23 01:35:42 +0100

    Add docs to AutoRegisterModules

--- src/main/java/com/toxicstoxm/LEDSuite/auto_registration/modules/AutoRegisterModules.java
+++ src/main/java/com/toxicstoxm/LEDSuite/auto_registration/modules/AutoRegisterModules.java
@@ -4,13 +4,27 @@ import com.toxicstoxm.LEDSuite.auto_registration.Registrable;
 import lombok.Getter;
 
 /**
- * Auto registrable modules.
+ * Enum representing auto-registrable modules in the application.
+ * These modules can be automatically discovered and registered during runtime.
+ *
  * @since 1.0.0
  * @see Registrable
  */
 @Getter
 public enum AutoRegisterModules {
+
+    /**
+     * Represents modules responsible for handling communication packets.
+     */
     PACKETS,
+
+    /**
+     * Represents modules that provide widgets for the user interface.
+     */
     WIDGETS,
-    ALERT_DIALOGS
+
+    /**
+     * Represents modules that handle alert dialogs in the application.
+     */
+    ALERT_DIALOGS;
 }

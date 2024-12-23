0a803378f761b8f82fc5c9b3bca949ebdba89c09
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-12-23 22:46:01 +0100

    Update Constants

--- src/main/java/com/toxicstoxm/LEDSuite/Constants.java
+++ src/main/java/com/toxicstoxm/LEDSuite/Constants.java
@@ -149,7 +149,7 @@ public class Constants {
                         public static final class AnimationList {
                             public static final String ICON = "icon";
                             public static final String LABEL = "label";
-                            public static final String PAUSEABLE = "pause_able";
+                            public static final String PAUSEABLE = "pauseable";
                         }
                     }
 

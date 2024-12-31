63dabf0be0e07e387a156453b98f09d87dfec32b
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-12-31 04:02:59 +0100

    Re-added MessageDialog, but for client messages

--- src/main/java/com/toxicstoxm/LEDSuite/ui/dialogs/alert_dialogs/MessageDialog.java
+++ src/main/java/com/toxicstoxm/LEDSuite/ui/dialogs/alert_dialogs/MessageDialog.java
@@ -1,26 +1,47 @@
 package com.toxicstoxm.LEDSuite.ui.dialogs.alert_dialogs;
 
-import com.toxicstoxm.LEDSuite.communication.packet_management.packets.message.ServerMessagePacket;
+import com.toxicstoxm.LEDSuite.ui.LEDSuiteApplication;
 import lombok.Builder;
+import org.gnome.adw.ResponseAppearance;
 import org.gnome.glib.GLib;
 import org.gnome.gtk.Widget;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
- * Message dialog which is used to display server messages to the user.
+ * Message dialog which is used to display client messages to the user.
  * @since 1.0.0
  */
 public class MessageDialog {
 
     private final AlertDialog<AlertDialogData> alertDialog;
+    private static final List<AlertDialogResponse> responses;
+
+    static {
+        responses = new ArrayList<>();
+
+        responses.add(
+                AlertDialogResponse.builder()
+                        .id(String.valueOf(UUID.randomUUID()))
+                        .label("Ok")
+                        .activated(true)
+                        .appearance(ResponseAppearance.SUGGESTED)
+                        .responseCallback(() -> LEDSuiteApplication.getLogger().debug("User acknowledged notification dialog."))
+                        .build()
+        );
+    }
+
 
     @Builder
-    public MessageDialog(@NotNull ServerMessagePacket packet) {
+    public MessageDialog(@NotNull MessageData messageData) {
         alertDialog = GeneralAlertDialog.create().configure(
                 AlertDialogData.builder()
-                        .heading(generateHeading(packet.getHeading(), packet.getSource()))
-                        .body(packet.getMessage())
-                        .responses(packet.getResponses())
+                        .heading(generateHeading(messageData.heading(), messageData.source()))
+                        .body(messageData.message())
+                        .responses(computeResponses(messageData.responses()))
                         .build()
         );
     }
@@ -38,9 +59,14 @@ public class MessageDialog {
         }
         if (heading != null && !heading.isBlank()) {
             sb.append(heading);
-        } else sb.append("Server Message");
+        } else sb.append("Client Message");
 
         return sb.toString();
+    }
+
+    private List<AlertDialogResponse> computeResponses(List<AlertDialogResponse> responses) {
+        if (responses != null && !responses.isEmpty()) return responses;
+        return MessageDialog.responses;
     }
 
     public void present(Widget parent) {

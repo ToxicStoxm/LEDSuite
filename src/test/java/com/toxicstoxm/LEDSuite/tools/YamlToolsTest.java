bb6ef886e1eef5f398cf5750a09e6b808bd1c520
ToxicStoxm <dominik0815oberlechner@gmail.com>
2025-04-27 23:14:46 +0200

    Improve YamlToolsTest

--- src/test/java/com/toxicstoxm/LEDSuite/tools/YamlToolsTest.java
+++ src/test/java/com/toxicstoxm/LEDSuite/tools/YamlToolsTest.java
@@ -1,10 +1,10 @@
 package com.toxicstoxm.LEDSuite.tools;
 
 import com.toxicstoxm.LEDSuite.communication.packet_management.DeserializationException;
+import com.toxicstoxm.LEDSuite.communication.packet_management.packets.errors.ErrorCode;
 import com.toxicstoxm.YAJL.YAJLManager;
 import com.toxicstoxm.YAJL.config.YAJLManagerConfig;
 import com.toxicstoxm.YAJSI.api.file.YamlConfiguration;
-import com.toxicstoxm.YAJSI.api.yaml.ConfigurationSection;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -24,73 +24,79 @@ class YamlToolsTest {
                         .build()
         );
 
-        assertDoesNotThrow(() ->
-            yaml.loadFromString(
-                    """
-                            StringTest: "String"
-                            IntegerTest: 5
-                            DoubleTest: 3.14
-                            LongTest: 435672378
-                            BooleanTest: true
-                            StringList:
-                              - "Hello"
-                              - " "
-                              - "World"
-                              - "!"
-                            ParentSection:
-                              ChildSection:
-                                ChildSectionValue: 8
-                            """
-            )
-        );
+        assertDoesNotThrow(() -> yaml.loadFromString(
+                """
+                StringTest: "String"
+                IntegerTest: 5
+                DoubleTest: 3.14
+                LongTest: 435672378
+                BooleanTest: true
+                StringList:
+                  - "Hello"
+                  - " "
+                  - "World"
+                  - "!"
+                ParentSection:
+                  ChildSection:
+                    ChildSectionValue: 8
+                """
+        ));
     }
 
     @Test
     void testCheckIfKeyExists() {
         assertTrue(YamlTools.checkIfKeyExists("StringTest", yaml));
-        ConfigurationSection section = yaml.getConfigurationSection("ParentSection");
-        assertNotNull(section);
-        assertTrue(YamlTools.checkIfKeyExists("ChildSection", section));
+        assertFalse(YamlTools.checkIfKeyExists("NonExistentKey", yaml));
     }
 
     @Test
     void testEnsureKeyExists() {
         assertDoesNotThrow(() -> YamlTools.ensureKeyExists("ParentSection", yaml));
-        assertThrows(DeserializationException.class, () -> YamlTools.ensureKeyExists("NotExistentSection", yaml));
+        DeserializationException ex = assertThrows(DeserializationException.class, () ->
+                YamlTools.ensureKeyExists("MissingKey", yaml));
+        assertEquals(ErrorCode.RequiredKeyIsMissing, ex.getErrorCode());
+    }
+
+    @Test
+    void testEnsureKeyExistsWithCustomErrorCode() {
+        ErrorCode customErrorCode = ErrorCode.Undefined;
+        DeserializationException ex = assertThrows(DeserializationException.class, () ->
+                YamlTools.ensureKeyExists("AnotherMissingKey", yaml, customErrorCode));
+        assertEquals(customErrorCode, ex.getErrorCode());
     }
 
     @Test
     void testGetStringIfAvailable() {
-        assertEquals("", YamlTools.getStringIfAvailable("StringTestNotExistent", yaml));
         assertEquals("String", YamlTools.getStringIfAvailable("StringTest", yaml));
+        assertEquals("", YamlTools.getStringIfAvailable("UnknownString", yaml));
+        assertEquals("Default", YamlTools.getStringIfAvailable("UnknownString", "Default", yaml));
     }
 
     @Test
     void testGetBooleanIfAvailable() {
-        assertFalse(YamlTools.getBooleanIfAvailable("BooleanTestNotExistent", yaml));
         assertTrue(YamlTools.getBooleanIfAvailable("BooleanTest", yaml));
+        assertFalse(YamlTools.getBooleanIfAvailable("UnknownBoolean", yaml));
+        assertTrue(YamlTools.getBooleanIfAvailable("UnknownBoolean", true, yaml));
     }
-
 
     @Test
     void testGetIntIfAvailable() {
-        assertEquals(0, YamlTools.getIntIfAvailable("IntegerTestNotExistent", yaml));
         assertEquals(5, YamlTools.getIntIfAvailable("IntegerTest", yaml));
+        assertEquals(0, YamlTools.getIntIfAvailable("MissingInt", yaml));
+        assertEquals(99, YamlTools.getIntIfAvailable("MissingInt", 99, yaml));
     }
 
     @Test
     void testGetDoubleIfAvailable() {
-        assertEquals(0, YamlTools.getDoubleIfAvailable("DoubleTestNotExistent", yaml));
         assertEquals(3.14, YamlTools.getDoubleIfAvailable("DoubleTest", yaml));
+        assertEquals(0.0, YamlTools.getDoubleIfAvailable("MissingDouble", yaml));
+        assertEquals(1.23, YamlTools.getDoubleIfAvailable("MissingDouble", 1.23, yaml));
     }
 
     @Test
     void testGetLongIfAvailable() {
-        assertEquals(0, YamlTools.getLongIfAvailable("LongTestNotExistent", yaml));
-        assertEquals(435672378, YamlTools.getLongIfAvailable("LongTest", yaml));
-    }
-
-    @Test
-    void testConstructIcon() {
+        assertEquals(435672378L, YamlTools.getLongIfAvailable("LongTest", yaml));
+        assertEquals(0L, YamlTools.getLongIfAvailable("MissingLong", yaml));
+        assertEquals(9876543210L, YamlTools.getLongIfAvailable("MissingLong", 9876543210L, yaml));
     }
 }
